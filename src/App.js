773650e61414c2998ae40f16500107860c26cc96
Mrilyes <ilyesghlila1@gmail.com>
2022-09-13 18:34:36 +0100

    CWD Building 4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { React , Component} from 'react';
 import CardList from './CardList'
-import {robots} from './robots'
+// import {robots} from './robots'
 import SearchBox from './SearchBox'
+import './App.css' ; 
+import 'tachyons'
 
 class App extends Component  {
   constructor(){
     super();
     // state is what change in the app / It's what describes the app.
     //the virtual Dom is just a javascript object the virtual Dom is just a new object that collects this entire state and re-act uses this state to render and pass them down as props to these components so that these components that are just pure functions can just render.
-
-
     this.state ={
-      robots : robots,
+      robots : [],
       searchField: ''
-    } 
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(usr => this.setState({robots:usr}));
   }
 
   onSearchChange = (event) => {
@@ -24,20 +30,24 @@ class App extends Component  {
     // console.log(filterRobots);
   }
   
-
   render(){   
       const filterRobots = this.state.robots.filter(robot => {
         return robot.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase())
       })
+      if (this.state.robots.length === 0) {
+        return <h1 className='tc'> Loading...</h1>
+        }
+    else {
     return (
-      <div className="tc">
-    <h1>RoboFriends</h1>
+    <div className="tc">
+    <h1 className='f1'>RoboFriends</h1>
     <SearchBox searchChange={this.onSearchChange} />
     <CardList robots={filterRobots} />
     </div>
   );
+  }
  }
 }
 export default App;
